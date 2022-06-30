d128fa38e8124d0a5afc46b88778d1a09a6d6e9f
cottonrose1011 <pjeka7er43@gmail.com>
2022-06-30 17:53:14 +0900

    initial commit

---
+++ SpringHckt/src/main/java/helloSpringHckt/SpringHcktApplication.java
@@ -0,0 +1,13 @@
+package helloSpringHckt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHcktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHcktApplication.class, args);
+	}
+
+}
